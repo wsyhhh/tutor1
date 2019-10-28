96bb6a820a72249ae0b86da84967e85fa3ba6366
Minhvuh <minhvuhvn@gmail.com>
2019-10-28 17:49:13 +0800

    'deploy'

--- src/data/topic2.js
+++ src/data/topic2.js
@@ -1,20 +1,20 @@
 export default [
-	{
-		question: 'What is the threshold for Kurtosis?',
-		correct_answer: '3',
-		incorrect_answers: [
-			'2',
-			'1',
-			'4'
-		]
-	},
-	{
-		question: 'What cannot be found using the describe function? IQR and ___',
-		correct_answer: 'IQR',
-		incorrect_answers: [
-			'skew',
-			'median',
-			'kurtosis'
-		]
-	},
+  {
+    question: 'What is the threshold for Kurtosis?',
+    correct_answer: '3',
+    incorrect_answers: [
+      '2',
+      '1',
+      '4'
+    ]
+  },
+  {
+    question: 'What cannot be found using the describe function? IQR and ___',
+    correct_answer: 'IQR',
+    incorrect_answers: [
+      'skew',
+      'median',
+      'kurtosis'
+    ]
+  }
 ]
