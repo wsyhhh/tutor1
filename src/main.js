ebdd08a2653e7196d647921f50843b1bbb168255
Minhvuh <minhvuhvn@gmail.com>
2019-11-10 23:22:16 +0800

    add codemirror

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueCodemirror from 'vue-codemirror'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'codemirror/lib/codemirror.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,6 +12,7 @@ import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueCodemirror)
 
 new Vue({
   router,
