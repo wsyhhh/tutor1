3244f8b90364c96557f90ec18fbb382bfe633aee
wsyhhh <32766063+wsyhhh@users.noreply.github.com>
2019-11-11 11:14:50 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: 'tutor1/',
-  baseUrl: 'tutor1/'
+  publicPath: process.env.NODE_ENV === 'production'
+        ? '/tutor1/'
+        : '/'
+  
 }
