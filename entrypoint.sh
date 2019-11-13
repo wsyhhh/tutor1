a789d4e0ddb5a8357770664795a173a9dc7aabbb
wsyhhh <32766063+wsyhhh@users.noreply.github.com>
2019-11-13 17:38:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -103,3 +103,26 @@ git push -f $REPOSITORY_PATH master:gh-pages
 # git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER ${BASE_BRANCH:-master}`:$BRANCH --force && \
 
 echo "Deployment succesful!"
+
+# start deploying to aws
+cd
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+export APINAME="$LAMBDA_FUNC_NAME-API"
+
+rm -f lambda-deploy.zip
+zip -r ./lambda-deploy.zip *
+pwd
+ls -ls .
+
+if sh deploy.sh
+    then 
+        exit 0
+    else
+        exit 1
+fi
+    
+exit 0
