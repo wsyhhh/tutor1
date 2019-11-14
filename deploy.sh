9d8f9fde288d2dae0e196ac3cc04cfc32edb0ad5
wsyhhh <32766063+wsyhhh@users.noreply.github.com>
2019-11-14 22:16:14 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #sam build
-sam package --output-template \
+sam package --output-template-file \
     packaged.yaml --s3-bucket mylambdacollectionwk7
 sam  deploy --template-file packaged.yaml \
     --region us-east-1 --capabilities \
-    CAPABILITY_IAM --stack-name samR3
+    CAPABILITY_IAM --stack-name samR4
