021727634254a2d99eec9a6d0651c9a66e0efe3c
wsyhhh <32766063+wsyhhh@users.noreply.github.com>
2019-11-15 16:51:48 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # sam build
 sam package --output-template-file \
     packaged.yaml --s3-bucket "${BUCKET_NAME}"
+echo "{$GIT_AUTHOR_NAME}.github.io/tutor1/"
 sam  deploy --template-file packaged.yaml \
     --region us-east-1 --capabilities \
     CAPABILITY_IAM --stack-name samR4\
