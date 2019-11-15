b07e435f0684b52c21cbe0c94e2a3a815a148979
Minhvuh <46840004+Minhvuh@users.noreply.github.com>
2019-11-15 19:04:39 +0800

    update lambda function

--- lambda/script.R
+++ lambda/script.R
@@ -5,8 +5,9 @@ handler <- function(...) {
     # multiValueQueryStringParameters,pathParameters,stageVariables,requestContext,body,isBase64Encoded]
     
     # print(requestInfo)
-    SIR = Sys.getenv("GIT_PAGE")
+    
     # print(requestInfo['httpMethod'])
+    SIR = Sys.getenv("GIT_PAGE")
     if(requestInfo['httpMethod'] == "GET") {
         return (list(
             statusCode = 301,
@@ -16,29 +17,36 @@ handler <- function(...) {
     }
     else if(requestInfo['httpMethod'] == "POST") {
         request = requestInfo["body"]
+        print("request")
         print(request)
         print(class(request))
-        print(length(request))
-        string <- toJSON(list(request))
-        print(string)
-        print(length(string))
+        string <- toJSON(request)
         num_char <- nchar(as.character(string))
         print(num_char)
+        print(string)
+        print(length(string))
+        # print("string")
+        # print(string)
+        code <- request["body"]
+        print("code")
+        print(code)
+        print(class(code))
+        print(length(code))
+        print(code[2])
+        # print(string)
+        # print(length(string))
+        # print(num_char)
 
-        if (num_char == 13) {
+        if (num_char == 11) {
             ev <- "Please key in your answer"
         } else {
-            res <- substr(string,12,num_char-4)
-            #res <- gsub("\n", "\\n", res)
-            print(res)
-            print(class(res))
             salary <- c(21000, 23400, 26800)
             employee <- c('John Doe','Peter Gynn','Jolie Hope')
             employ.data <- data.frame(employee, salary)
             m <- mean(employ.data$salary)
             print(m)
-            ex <- expression(res)
-            test <- eval(parse(text=res))
+            # ex <- expression(code)
+            test <- eval(parse(text=code))
             print(test)
             print(class(test))
             if (is.na(m == test)) {
