ab641efd35642dd84496bc5b320f5fe55731615f
wsyhhh <32766063+wsyhhh@users.noreply.github.com>
2019-11-15 17:28:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 # sam build
 sam package --output-template-file \
     packaged.yaml --s3-bucket "${BUCKET_NAME}"
-echo "{$GIT_AUTHOR_NAME}.github.io/tutor1/"
+echo "${GIT_AUTHOR_NAME}.github.io/tutor1/"
 sam  deploy --template-file packaged.yaml \
+    --parameter-overrides "GIT_PAGE=${GIT_AUTHOR_NAME}.github.io/tutor1/"\
     --region us-east-1 --capabilities \
-    CAPABILITY_IAM --stack-name samR4\
-    --parameter-overrides GIT_PAGE="{$GIT_AUTHOR_NAME}.github.io/tutor1/"
+    CAPABILITY_IAM --stack-name samR4
+    
